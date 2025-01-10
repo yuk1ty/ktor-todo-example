a2e3a20ffe4ae71eae23a371a9395990d238766f
yuk1ty <yuk1ty@users.noreply.github.com>
2025-01-11 00:07:45 +0900

    Separate establishDatabaseConnection from Application

--- src/main/kotlin/com/github/yuk1ty/todoAppKt/shared/modules/Database.kt
+++ src/main/kotlin/com/github/yuk1ty/todoAppKt/shared/modules/Database.kt
@@ -3,10 +3,9 @@ package com.github.yuk1ty.todoAppKt.shared.modules
 import com.github.yuk1ty.todoAppKt.adapter.database.DatabaseConn
 import com.github.yuk1ty.todoAppKt.adapter.database.Permission
 import com.github.yuk1ty.todoAppKt.shared.config.AppConfig
-import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.Database
 
-internal fun Application.establishDatabaseConnection(cfg: AppConfig): Pair<DatabaseConn<Permission.ReadOnly>, DatabaseConn<Permission.Writable>> {
+internal fun establishDatabaseConnection(cfg: AppConfig): Pair<DatabaseConn<Permission.ReadOnly>, DatabaseConn<Permission.Writable>> {
     val read = cfg.readableDatabase.run {
         DatabaseConn.establishRead(Database.connect(jdbcUrl, driver = driverName, user = user, password = password))
     }
